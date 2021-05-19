1889bdd24e9cbce9b20c37226d3060a51a09484a
edwin4v@gmail.com <edwin4v@gmail.com>
2021-05-19 15:38:19 +0800

    Added deployment code for testing

---
+++ deployment/app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+from turbojpeg import TurboJPEG
+import tensorflow as tf
+import cv2
+
+import os
+os.chdir('..')
+
+# Initialize necessary components
+app = FastAPI()
+turbojpeg = TurboJPEG()
+model = tf.saved_model.load('weights/mobilenet_hd/')
+preproc_model = tf.saved_model.load('weights/preproc/')
+
+infer = model.signatures['serving_default']
+preproc = preproc_model.signatures['serving_default']
+
+labels = {
+    0: 'not hot dog',
+    1: 'hot dog'
+}
+
+@app.post('/image')
+async def get_index(file: UploadFile = File(...)):
+    contents = await file.read()
+    img_buffer = np.frombuffer(contents, dtype=np.uint8)
+    img = turbojpeg.decode(img_buffer)
+    # img = cv2.imdecode(img_buffer, cv2.IMREAD_COLOR)
+    
+    img_pre = tf.expand_dims(img, axis=0)
+    img_pre = preproc(img_pre)['output_0']
+    preds = infer(img_pre)['outputs']
+    
+    return {
+        'prediction': labels[preds.numpy().argmax()],
+        'score': round(float(preds.numpy().max()) * 100, 2)
+    }
---
+++ deployment/benchmark.py
@@ -0,0 +1,31 @@
+import httpx
+import time
+import asyncio
+import glob
+
+import os
+os.chdir('..')
+
+async def runpost(image):
+    async with httpx.AsyncClient() as client:
+        results = await client.post(
+            'http://localhost:8000/image', 
+            files={'file': open(image, 'rb')}, timeout=None
+        )
+            
+    return results.json()
+
+async def get_all(images):
+    requests = []
+    for image in images:
+        requests.append(runpost(image))
+        
+    results = await asyncio.gather(*requests)
+    return results
+    
+start_time = time.time()
+images = glob.glob('data/test/**/*.jpg', recursive=True)
+asyncio.run(get_all(images))
+end_time = time.time() - start_time
+
+print('Took: {} for: {} images'.format(round(end_time, 4), len(images)))
